87f072de493c1167e17c9440335faf561d36f341
Seu Nome <seuemail@email.com>
2026-01-08 10:48:42 -0400

    Initial commit - Finance API backend with Flask

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from .config import Config
+from .extensions import db, jwt
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from .routes.auth_routes import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    with app.app_context():
+        db.create_all()
+
+    return app
